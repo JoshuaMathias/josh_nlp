259d1e550857aa2806c3da56d5dec0cf3ba9e675
JoshuaMathias <emathias@patas.ling.washington.edu>
2018-02-22 15:37:23 -0800

    test

--- tests/classifiers_tests.py
+++ tests/classifiers_tests.py
@@ -34,17 +34,35 @@ from josh_nlp import classifiers
 #         self.assertEqual('{0:.5f}'.format(categories[1].getFeatureWeight(featI)), "0.00686")
 #         self.assertEqual('{0:.5f}'.format(categories[2].getFeatureWeight(featI)), "-0.03754")
 
-class NaiveBayesBinaryTestCase(TestCase):
+# class NaiveBayesBinaryTestCase(TestCase):
+#     def setUp(self):
+#         self.train_filename = "data/train_10.vectors.txt"
+#         self.test_filename = "data/test_10.vectors.txt"
+#         self.naiveBayes = classifiers.NaiveBayes(2)
+#         train_lines = open(self.train_filename,'r').readlines()
+#         self.naiveBayes.train(training_data, cond_prob_delta, class_prior_delta)
+#         return
+
+#     def test_test(self):
+#         train_answers, train_predicted, nextSys = nb.test(training_data)
+
+class TBLTestCase(TestCase):
     def setUp(self):
         self.train_filename = "data/train_10.vectors.txt"
         self.test_filename = "data/test_10.vectors.txt"
-        self.naiveBayes = classifiers.NaiveBayes(2)
+        self.tbl = classifiers.TBL()
         train_lines = open(self.train_filename,'r').readlines()
-        self.naiveBayes.train(training_data, cond_prob_delta, class_prior_delta)
+        min_gain = 1
+        self.tbl.train(train_lines, min_gain)
         return
 
-    def test_test(self):
-        train_answers, train_predicted, nextSys = nb.test(training_data)
+    # def test_test(self):
+    #     train_answers, train_predicted, nextSys = self.tbl.test(self.test_lines)
+
+    def test_train(self):
+        # Test that data is initialized correctly.
+        # All features should have the label of the first class that appears in the training data.
+        
 
 
 unittest.main()
